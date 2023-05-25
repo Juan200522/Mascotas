a010924de29685391c74363a123f1a9d06d688b0
Juan <114256894+Juan200522@users.noreply.github.com>
2023-05-25 16:10:44 -0500

    Add files via upload

---
+++ src/mascotas/Principal.java
@@ -0,0 +1,15 @@
+package mascotas;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Mascotas misMascotas = new Mascotas();
+
+        misMascotas.ingresar();
+        misMascotas.mostrarInformacion();
+        misMascotas.mostrarInformacionGeneral();
+        misMascotas.consultaIndividual();
+
+	}
+
+}
